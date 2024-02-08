17b123104483696d5fc5059d5710624a3b736c37
Carlos Alberto Lira <47789867+carlosalberto05@users.noreply.github.com>
2024-02-07 21:28:21 -0600

    first commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,31 @@
+import { getImageUrl } from "../../utils";
+import styles from "./Hero.module.css";
+
+const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I&apos;m Carlos</h1>
+        <p className={styles.description}>
+          I&apos;m a web developer with 2 years of experience using React and
+          Node JS.
+        </p>
+        <a
+          href="mailto:carlos_alberto-lira@outlook.com"
+          className={styles.contactBtn}
+        >
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
+
+export default Hero;
