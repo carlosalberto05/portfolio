091b7db528138c1d7a08145841ec2108190f3922
Carlos Alberto Lira <47789867+carlosalberto05@users.noreply.github.com>
2024-02-12 17:03:16 -0600

    contact section

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,37 @@
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel fre to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:carlos_alberto-lira@outlook.com">
+            carlos_alberto-lira@outlook.com
+          </a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/carlos-alberto-lira/">
+            https://www.linkedin.com/in/carlos-alberto-lira
+          </a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/carlosalberto05">
+            https://github.com/carlosalberto05
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Contact;
